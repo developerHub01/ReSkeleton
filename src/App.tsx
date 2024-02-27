1e5d04996804c89aaf67303a4d00991b371542e3
shaki <developerhubytchannel@gmail.com>
2024-02-28 01:03:35 +0600

    structure ready

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,14 @@
+import Skeleton from "./Components/Skeleton";
+
 const App = () => {
-  return <div></div>;
+  return (
+    <div>
+      <Skeleton width={300} />
+      <Skeleton type="round" />
+      <Skeleton type="react" height={500} roundness={50} />
+      <Skeleton type="circle" width={50} size={"100"} color="#001219" />
+    </div>
+  );
 };
 
 export default App;
--- src/Components/Skeleton.tsx
+++ src/Components/Skeleton.tsx
@@ -1,25 +1,140 @@
-import React from "react";
-import SkeletonText from "./SkeletonText";
-import SkeletonCircle from "./SkeletonCircle";
-import SkeletonRect from "./SkeletonRect";
-import SkeletonRound from "./SkeletonRound";
+import styled, { StyleSheetManager, css } from "styled-components";
+
+type stringOrNum = string | number;
 
 interface SkeletonProps {
-  type: "text" | "circle" | "react" | "round";
+  type?: "text" | "circle" | "react" | "round";
+  size?: stringOrNum;
+  width?: stringOrNum;
+  height?: stringOrNum;
+  color?: string;
+  animType?: string | boolean;
+  roundness?: string | number;
+  className?: string | object;
+  style?: object;
 }
 
-const Skeleton: React.FC<SkeletonProps> = (props) => {
-  const { type } = props;
-  switch (type.toLowerCase()) {
+const checkPrefixAndModify = (value: stringOrNum, prefix: string) => {
+  return (
+    typeof value === "string"
+      ? isNaN(+value)
+        ? value
+        : +value + prefix
+      : value + prefix
+  ).trim();
+};
+
+const checkRoundness = (type: string) => {
+  switch (type?.toLowerCase()) {
     case "circle":
-      return <SkeletonCircle />;
+      return "50%";
     case "react":
-      return <SkeletonRect />;
+      return "0";
     case "round":
-      return <SkeletonRound />;
+      return "12px";
     default:
-      return <SkeletonText />;
+      return "4px";
   }
+};
+const slideAnim = css`
+  0% {
+    left: 0;
+    opacity: 1;
+  }
+  20%,
+  80% {
+    opacity: 0;
+  }
+  50% {
+    opacity: 1;
+  }
+  100% {
+    opacity: 1;
+    left: 200%;
+  }
+`;
+const pulseAnim = css`
+  0%,
+  100% {
+    opacity: 0.3;
+    transform: scaleX(0.99);
+  }
+  50% {
+    opacity: 0.85;
+    transform: scaleX(1);
+  }
+`;
+
+const SkeletonComp = styled.div<SkeletonProps>`
+  background: ${(props) => props.color};
+  ${(props) =>
+    props.type?.toLowerCase() === "circle"
+      ? `width: ${props.size};
+    height: ${props.size};`
+      : `width: ${props.width};
+    height: ${props.height};`}
+  margin: 5px 0;
+  border-radius: ${(props) => props.roundness};
+  overflow: hidden;
+  position: relative;
+  ${(props) =>
+    props.animType ? `animation: ${pulseAnim} 1.6s linear infinite;` : ""}
+  transform-origin: left center;
+  &::before,
+  &::after {
+    content: "";
+    position: absolute;
+    width: 20%;
+    height: 5000%;
+    top: 50%;
+    transform: translate(-100%, -50%) rotate(15deg);
+    background: rgba(255, 255, 255, 0.25);
+    left: 0;
+    filter: blur(50px);
+  }
+  ${(props) =>
+    typeof props.animType === "string" &&
+    props.animType?.toLowerCase() === "slide"
+      ? `&::before {
+      animation: ${slideAnim} 1.5s linear infinite;
+    }
+    &::after {
+      animation-delay: 0.5s;
+      animation: ${slideAnim} 1s linear infinite;
+    }`
+      : ""}
+`;
+const Skeleton: React.FC<SkeletonProps> = (props) => {
+  const newModifiedProps = { ...props };
+  newModifiedProps["animType"] || (newModifiedProps["animType"] = "pulse");
+  newModifiedProps["type"] || (newModifiedProps["type"] = "text");
+  newModifiedProps["color"] || (newModifiedProps["color"] = "#ddd");
+
+  newModifiedProps["size"] = checkPrefixAndModify(
+    newModifiedProps["size"] || "20px",
+    "px"
+  );
+  newModifiedProps["width"] = checkPrefixAndModify(
+    newModifiedProps["width"] || "100%",
+    "px"
+  );
+  newModifiedProps["height"] = checkPrefixAndModify(
+    newModifiedProps["height"] || "16px",
+    "px"
+  );
+  newModifiedProps["roundness"] =
+    newModifiedProps["roundness"] || checkRoundness(newModifiedProps["type"]);
+
+  newModifiedProps["roundness"] = checkPrefixAndModify(
+    newModifiedProps["roundness"],
+    "px"
+  );
+
+  return (
+    // <StyleSheetManager shouldForwardProp={(props) => props !== "animType"}>
+    <SkeletonComp {...newModifiedProps} />
+    // </StyleSheetManager>
+  );
 };
 
 export default Skeleton;
